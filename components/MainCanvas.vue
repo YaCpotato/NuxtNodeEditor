aa1f0f3a3530f6f6b87b5caecf9bbaa23436b2ed
YaCpotato <yasshisshy@gmail.com>
2020-03-25 20:33:55 +0900

    複数対応

--- components/MainCanvas.vue
+++ components/MainCanvas.vue
@@ -6,6 +6,7 @@
       width="80vw"
       height="80vh"
       xmlns="http://www.w3.org/2000/svg"
+      @click="drawSomething"
       @mousemove="mouseMove($event)"
     ></svg>
   </div>
@@ -26,32 +27,6 @@ export default {
       classNameLength: 8,
       classNameString: 'abcdefghijkmlnopqrstuvwxyz'
     }
-  },
-  mounted() {
-    let hash = ''
-    for (let i = 0; i < this.classNameLength; i++) {
-      hash += this.classNameString[
-        Math.floor(Math.random() * this.classNameString.length)
-      ]
-    }
-    d3.select('.draw-area')
-      .append('rect')
-      .attr('class', hash)
-      .attr('x', this.mouseX)
-      .attr('y', this.mouseY)
-      .attr('width', 100)
-      .attr('height', 80)
-    d3.drag('.' + hash)
-      .on('start', this.dragstarted)
-      .on('drag', this.dragged)
-      .on('end', this.dragended)
-    const dragHandler = d3.drag().on('drag', function() {
-      d3.select(this)
-        .attr('x', d3.event.x)
-        .attr('y', d3.event.y)
-    })
-
-    dragHandler(d3.select('.draw-area').selectAll('rect'))
   },
   methods: {
     mouseMove(e) {
@@ -81,6 +56,13 @@ export default {
         .on('start', this.dragstarted)
         .on('drag', this.dragged)
         .on('end', this.dragended)
+      const dragHandler = d3.drag().on('drag', function() {
+        d3.select(this)
+          .attr('x', d3.event.x)
+          .attr('y', d3.event.y)
+      })
+
+      dragHandler(d3.select('.' + hash))
     }
   }
 }
