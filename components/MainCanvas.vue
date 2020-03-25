5fa68760161d5a94a38f514d9873472689c5e182
YaCpotato <yasshisshy@gmail.com>
2020-03-25 20:30:03 +0900

    ドラッグ&ドロップ再現

--- components/MainCanvas.vue
+++ components/MainCanvas.vue
@@ -6,7 +6,6 @@
       width="80vw"
       height="80vh"
       xmlns="http://www.w3.org/2000/svg"
-      @click="drawSomething"
       @mousemove="mouseMove($event)"
     ></svg>
   </div>
@@ -22,8 +21,37 @@ export default {
     return {
       type: 'rect',
       mouseX: null,
-      mouseY: null
+      mouseY: null,
+      mouseDowned: false,
+      classNameLength: 8,
+      classNameString: 'abcdefghijkmlnopqrstuvwxyz'
     }
+  },
+  mounted() {
+    let hash = ''
+    for (let i = 0; i < this.classNameLength; i++) {
+      hash += this.classNameString[
+        Math.floor(Math.random() * this.classNameString.length)
+      ]
+    }
+    d3.select('.draw-area')
+      .append('rect')
+      .attr('class', hash)
+      .attr('x', this.mouseX)
+      .attr('y', this.mouseY)
+      .attr('width', 100)
+      .attr('height', 80)
+    d3.drag('.' + hash)
+      .on('start', this.dragstarted)
+      .on('drag', this.dragged)
+      .on('end', this.dragended)
+    const dragHandler = d3.drag().on('drag', function() {
+      d3.select(this)
+        .attr('x', d3.event.x)
+        .attr('y', d3.event.y)
+    })
+
+    dragHandler(d3.select('.draw-area').selectAll('rect'))
   },
   methods: {
     mouseMove(e) {
@@ -36,11 +64,23 @@ export default {
       }
     },
     drawSomething() {
+      let hash = ''
+      for (let i = 0; i < this.classNameLength; i++) {
+        hash += this.classNameString[
+          Math.floor(Math.random() * this.classNameString.length)
+        ]
+      }
       d3.select('.draw-area')
-        .append('text')
+        .append('rect')
+        .attr('class', hash)
         .attr('x', this.mouseX)
         .attr('y', this.mouseY)
-        .text('Hello!')
+        .attr('width', 100)
+        .attr('height', 80)
+      d3.drag('.' + hash)
+        .on('start', this.dragstarted)
+        .on('drag', this.dragged)
+        .on('end', this.dragended)
     }
   }
 }
